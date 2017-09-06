3a5ed7542d2da1f725a71e470aab24b815ec0f34
evgG <evgueniy.g@gmail.com>
2017-09-06 18:10:29 +0300

    routing_v3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,29 @@ import { AppComponent }        from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent }     from './heroes.component';
 import { HeroService }         from './hero.service';
+import { DashboardComponent }  from './dashboard.component';
 
 @NgModule({
 	imports:      [ BrowserModule,
-		FormsModule,
-		RouterModule.forRoot([
-			{
-				path: 'heroes',
-				component: HeroesComponent
-			}
-		])
-	],
-	declarations: [ AppComponent, HeroDetailComponent, HeroesComponent ],
+									FormsModule,
+									RouterModule.forRoot([
+										{
+											path: 'heroes',
+											component: HeroesComponent
+										},
+										{
+											path: 'dashboard',
+											component: DashboardComponent
+										},
+										{
+											path: '',
+											redirectTo: '/dashboard',
+											pathMatch: 'full'
+										},
+									])
+								],
+	declarations: [ AppComponent, HeroDetailComponent,
+									HeroesComponent, DashboardComponent ],
 	providers:    [ HeroService ],
 	bootstrap:    [ AppComponent ]
 })
